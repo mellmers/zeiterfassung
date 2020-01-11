b367fd8437b6c0c5695247924ce312d3606f4987
Moritz Ellmers <moritz.ellmers@web.de>
2020-01-11 13:54:30 +0100

    frontend: added duration in working times table

--- frontend/src/routes/zeiterfassung/index.js
+++ frontend/src/routes/zeiterfassung/index.js
@@ -271,21 +271,46 @@ export default class Zeiterfassung extends Component {
                         <th>Datum</th>
                         <th>Von</th>
                         <th>Bis</th>
+                        <th>Dauer</th>
                     </tr>
                 </thead>
                 <tbody>
                     {workingTimes.map( (wT, index) => {
                         let start = new Date(wT.start.time),
-                            end = null;
+                            end = null,
+                            durationString = '-';
+                        
                         if (wT.end && wT.end.time) {
                             end = new Date(wT.end.time);
+
+                            // Dauer berechnen (Zeitdifferenz zwischen Start und Ende)
+                            let timeDifference = end - start,
+                                duration = {};
+
+                            let secondsInADay = 60 * 60 * 1000 * 24,
+                                secondsInAHour = 60 * 60 * 1000;
+
+                            duration.hours = Math.floor(timeDifference / secondsInAHour);
+                            duration.minutes = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000));
+                            duration.seconds = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000);
+
+                            durationString = '';
+                            if (duration.hours > 0) {
+                                durationString += duration.hours + ' h ';
+                            }
+                            if (duration.hours > 0 || duration.minutes > 0) {
+                                durationString += duration.minutes + ' m ';
+                            }
+                            durationString += duration.seconds + ' s';
                         }
+
                         return (
                             <tr key={index}>
                                 <td>{days[start.getDay()]}{end && start.getDay() !== end.getDay() ? ' - ' + days[end.getDay()] : null}</td>
                                 <td>{start.toLocaleDateString()}{end && start.toLocaleDateString() !== end.toLocaleDateString() ? ' - ' + end.toLocaleDateString() : null}</td>
                                 <td>{start.toLocaleTimeString()}</td>
                                 <td>{end ? end.toLocaleTimeString() : '-'}</td>
+                                <td style={{textAlign: 'right'}}>{durationString.replace(/ /g, '\u00a0')}</td>
                             </tr>
                         )
                     })}
